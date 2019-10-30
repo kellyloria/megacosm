89861cf263cc247121115fbd6dc659e4a0d96604
kellyloria <kelo2727@colorado.edu>
2019-10-30 16:30:16 -0600

    Updated the difference in DOC between treatment and control "mega cosoms" in 2019 pilot of leaf packs at Soddie.

--- SD_DOC_WaterQuality.R
+++ SD_DOC_WaterQuality.R
@@ -1,5 +1,10 @@
+# Water quality data from Soddie pilots in 2019
 
-d <- read.csv(file.choose())
+library(tidyverse)
+library(lubridate)
+library(MuMIn)
+
+#d <- read.csv(file.choose())
 #
 names(d)
 d$amount
@@ -109,3 +114,72 @@ summary(DOCmod1)
 #################################################
 ## LOOK at tanks!
 ## shape for whether DOC was pulled from tank
+
+
+
+
+######################
+# Soddie 2019 DOC data
+
+doc <- read.csv("2019_DOCpilot.csv")
+names(doc)
+
+# fix and add in more temporal parameters 
+doc$date1 <- as.Date.character(doc$date, format="%m/%d/%y")
+range(doc$date1)
+
+#gl4.zo <- transform(gl4.zoo2, ndate = as.numeric(date),
+#                    year = as.numeric(format(date, '%Y')),
+#                    nmonth = as.numeric(format(date, '%m')),
+#                    doy    = as.numeric(format(date, '%j')))
+
+#gl4.zo$weekn <- week(gl4.zo$date)
+#gl4.zo$weekny <- paste(gl4.zo$weekn, gl4.zo$year)
+
+# restrict to 7/24/19
+doc.0 <- doc %>% 
+  subset(date1 == "2019-07-10")
+
+doc.1 <- doc %>% 
+  subset(date1 == "2019-07-24")
+
+# restrict to after 7/24/19
+doc.2 <- doc %>% 
+  subset(date1 == "2019-08-19")
+
+se <- function(x) sqrt(var(x)/length(x))
+
+DOC_TC0 <- aggregate(DOC ~ treatment, data=doc.0, FUN=mean) 
+DOC_TC0$SE <- aggregate(DOC ~ treatment, data=doc.0, FUN=se)[,2]
+DOC_TC0$date <- "2019-07-10"
+
+DOC_TC1 <- aggregate(DOC ~ treatment, data=doc.1, FUN=mean) 
+DOC_TC1$SE <- aggregate(DOC ~ treatment, data=doc.1, FUN=se)[,2]
+DOC_TC1$date <- "2019-07-19"
+
+DOC_TC2 <- aggregate(DOC ~ treatment, data=doc.2, FUN=mean) 
+DOC_TC2$SE <- aggregate(DOC ~ treatment, data=doc.2, FUN=se)[,2]
+DOC_TC2$date <- "2019-08-06"
+
+doc_2 <- rbind(DOC_TC1, DOC_TC2)
+
+p <- ggplot(doc_2, aes(x=date, y=DOC, fill=treatment)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=DOC-SE, ymax=DOC+SE), width=0,
+                position=position_dodge(.9)) +
+  scale_y_continuous(name="DOC (mg/L)", limits=c(0, 9), breaks=seq(0, 9, 1.5))
+
+p + scale_fill_manual(values=c("dodgerblue2", "darkseagreen4")) + theme_classic() + 
+  theme(axis.text.x = element_text(vjust = 1.0),  
+        panel.border = element_rect(linetype = "solid", fill = NA))
+
+#ggsave("DOC_pot.jpg",
+#       scale = 2, width = 5, height = 4, units = c("cm"),
+#       dpi = 300)
+
+
+
+
+
+
+
--- Zoop_Mortality.R
+++ Zoop_Mortality.R
@@ -25,6 +25,55 @@ mortplot <- ggplot(data = d, aes(x = timestamp1, y = per_dead,
   theme(axis.text.x = element_text(angle = 25, vjust = 1.0, hjust = 1.0)) + 
   facet_grid(Treatment~.)
 
-ggsave(filename = "19mortalplot.jpeg", mortplot, scale = 0.75, width = 40,
-       height = 20, units = c("cm"),
-       dpi = 300)
+#not much of a trend:
+morttempplot <- ggplot(data = d, aes(x = Temperature, y = total.dead,
+                                 color =as.factor(Incubator), shape=as.factor(Treatment))) +
+  geom_point() + labs( x = "Temp", y = "Number dead")  
+
+#ggsave(filename = "19mortalplot.jpeg", mortplot, scale = 0.75, width = 40,
+#       height = 20, units = c("cm"),
+#       dpi = 300)
+
+
+## Traits ##
+e <- read.csv("Zoop_mortalitytraits.csv")
+names(e)
+
+
+# plot size and TOD
+sizeTODplot <- ggplot(data = e, aes(x = observation.number, y = Size_mm,
+                                 color =as.factor(Sex), shape=as.factor(Sex))) +
+  geom_point() + labs( x = "Observation number", y = "Size (mm)") +
+  #geom_jitter(width = 0.18, height = 0) + 
+  theme_bw() 
+
+sizeTODplot <- ggplot(data = e, aes(x = observation.number, y = total_dead,
+                                    color =as.factor(Sex), shape=as.factor(Sex))) +
+  geom_point() + labs( x = "Observation number", y = "Size (mm)") +
+  #geom_jitter(width = 0.18, height = 0) + 
+  theme_bw() 
+
+# bar plot of sex of individuals dead at each observation  
+sizeTODplot <- ggplot(data = e, aes(x = observation.number, y=number_dead)) +
+  geom_bar(fill = Sex) + labs( x = "Observation number", y = "Size (mm)") +
+  #geom_jitter(width = 0.18, height = 0) + 
+  theme_bw() 
+
+ggplot(data=e, aes(x=observation.number, y=number_dead, fill=Sex)) +
+  geom_bar(stat="identity") + scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +
+  theme_minimal()
+
+
+## Sex based traits ##
+f <- read.csv("zoop_mortalitysex.csv")
+names(f)
+
+sizeTODplot <- ggplot(data = f) +
+  geom_point(aes(x = observation.number, y = F_deadT_jitter), color="#8c4553") + 
+  geom_line(aes(x = observation.number, y = F_deadT_jitter), color="#8c4553") +
+  geom_point(aes(x = observation.number, y = G_deadT_jitter), color="#a6858c") + 
+  geom_line(aes(x = observation.number, y = G_deadT_jitter), color="#a6858c") +
+  geom_point(aes(x = observation.number, y = M_deadT), color="#466782") + 
+  geom_line(aes(x = observation.number, y = M_deadT), color="#466782") +
+  labs( x = "Observation number", y = "Number dead") + 
+  theme_bw() 
